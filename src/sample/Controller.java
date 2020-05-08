9672567242ebfd9c467fe2598c9c009807391da3
connorcopus <ccopus1297@gmail.com>
2020-05-08 17:25:30 -0400

    Start of user UI and adding DB info to GUI

---
+++ src/sample/Controller.java
@@ -0,0 +1,6 @@
+package sample;
+
+
+// Used to add functionality to button and search bar in user interface
+public class Controller {
+}
---
+++ src/sample/Displayer.java
@@ -0,0 +1,23 @@
+package sample;
+
+import javafx.fxml.FXML;
+import javafx.fxml.Initializable;
+import javafx.scene.chart.XYChart;
+import javafx.scene.chart.LineChart;
+import javafx.scene.chart.NumberAxis;
+import javafx.scene.chart.PieChart;
+import javafx.scene.text.Text;
+import java.net.URL;
+import java.sql.SQLException;
+import java.util.ArrayList;
+import java.util.ResourceBundle;
+
+    public class Displayer implements Initializable {
+
+
+        @Override
+        public void initialize(URL url, ResourceBundle resourceBundle) {
+
+        }
+    }
+
