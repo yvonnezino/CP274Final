bdb448097bc8dc7a8c1389e0b68c2dcb4981b9bb
Mac O'Brien <mac.w.obrien@gmail.com>
2020-05-08 14:41:38 -0500

    fixed DbTools for new dataset

--- src/sample/County.java
+++ src/sample/County.java
@@ -1,19 +1,41 @@
 package sample;
 
-public class County {
-    private int fips;
-    private String name;
-    private int confirmed;
-    private int deaths;
-    private int recovered;
+import jdk.jshell.spi.ExecutionControlProvider;
 
-    public County(int fips, String name, int confirmed, int deaths, int recovered){
+import java.util.ArrayList;
+import java.util.HashMap;
+
+public class County {
+    private double fips;
+    private String name;
+    private HashMap<String, Integer> confirmed;
+    private HashMap<String, Integer> deaths;
+
+    public County(double fips, String name,  HashMap<String,Integer> confirmed, HashMap<String,Integer> deaths){
         this.fips = fips;
         this.name = name;
         this.confirmed = confirmed;
         this.deaths = deaths;
-        this.recovered = recovered;
+    }
+    //specify deaths or confirmed with value parameter
+    public int getByDate(String value, String date) throws Exception {
+        if(value.equals("confirmed")){
+            return confirmed.get(date);
+        } else if(value.equals("deaths")){
+            return deaths.get(date);
+        }
+        else{
+            throw new Exception("Value must be \"confirmed\" or \"deaths\"");
+        }
     }
 
-
+    public ArrayList<Integer> getAll(String value) throws Exception {
+        if(value.equals("confirmed")){
+            return new ArrayList<Integer>(confirmed.values());
+        }else if(value.equals("deaths")){
+            return new ArrayList<Integer>(deaths.values());
+        }else{
+            throw new Exception("Value must be \"confirmed\" or \"deaths\"");
+        }
+    }
 }
--- src/sample/DbTools.java
+++ src/sample/DbTools.java
@@ -5,12 +5,14 @@ import java.sql.DriverManager;
 import java.sql.PreparedStatement;
 import java.sql.ResultSet;
 import java.util.ArrayList;
-//Tools for getting data from the database
+import java.util.HashMap;
+
+//Tools for getting data from the database (only works for US data right now (if it works at all))
 public class DbTools {
     public static Connection getConnection() throws Exception {
         Connection conn = null;
         // db parameters
-        String url = "jdbc:sqlite:LatestDataSet.db";
+        String url = "jdbc:sqlite:TimeDependent.db";
         // create a connection to the database
         conn = DriverManager.getConnection(url);
 
@@ -18,28 +20,25 @@ public class DbTools {
 
         return conn;
     }
-    //Returns all counties
+
+    //Returns all US counties with daily confirmed and death values included
     public static ArrayList<County> getAllCounties() throws Exception {
         Connection con = getConnection();
-        PreparedStatement ps = con.prepareStatement("SELECT fips, Admin2, Confirmed, Deaths," +
-                " Recovered FROM LatestDataSet");
+        PreparedStatement ps = con.prepareStatement("SELECT fips, Admin2 FROM DeathsUS");
         ResultSet rs = ps.executeQuery();
         ArrayList<County> result = new ArrayList<County>();
         while (rs.next()) {
-            if(rs.getString("Admin2") != null) {
-                result.add(new County(rs.getInt("fips"), rs.getString("Admin2"),
-                        rs.getInt("Confirmed"), rs.getInt("Deaths"),
-                        rs.getInt("Recovered")));
-            }
+            result.add(new County(rs.getDouble("FIPS"), rs.getString("Admin2"),
+                    getByFips("ConfirmedUS", rs.getDouble("FIPS")),
+                    getByFips("DeathsUS", rs.getDouble("FIPS"))));
         }
         rs.close();
         return result;
     }
-    //not done
-    //if there are multiple countries with provinces of the same name, this might not work
+
     public static ArrayList<State> getAllStates() throws Exception {
         Connection con = getConnection();
-        PreparedStatement ps = con.prepareStatement("SELECT Province_State FROM LatestDataSet");
+        PreparedStatement ps = con.prepareStatement("SELECT Province_State FROM DeathsUS");
         ResultSet rs = ps.executeQuery();
 
         ArrayList<State> result = new ArrayList<State>();
@@ -57,21 +56,49 @@ public class DbTools {
 
     private static ArrayList<County> getCountiesByStateName(String stateName) throws Exception {
         Connection con = getConnection();
-        PreparedStatement ps = con.prepareStatement("SELECT fips, Admin2, Confirmed, Deaths," +
-                " Recovered, Province_State FROM LatestDataSet");
+        PreparedStatement ps = con.prepareStatement("SELECT * FROM DeathsUS");
         ResultSet rs = ps.executeQuery();
         ArrayList<County> result = new ArrayList<County>();
         while(rs.next()){
             if(rs.getString("Province_State").equals(stateName)){
-                result.add(new County(rs.getInt("fips"), rs.getString("Admin2"),
-                        rs.getInt("Confirmed"), rs.getInt("Deaths"),
-                        rs.getInt("Recovered")));
+                result.add(new County(rs.getDouble("FIPS"), rs.getString("Admin2"),
+                        getByFips("ConfirmedUS", rs.getDouble("FIPS")),
+                        getByFips("DeathsUS", rs.getDouble("FIPS"))));
             }
         }
 
         return result;
     }
+    //returns a hashmap containing each day linked to the corresponding value for the specified county
+    //specify deaths or confirmed cases with field variable
+    private static HashMap<String, Integer> getByFips(String field, double fips) throws Exception {
+        Connection con = getConnection();
+        PreparedStatement ps = con.prepareStatement("SELECT * FROM " + field);
+        ResultSet rs = ps.executeQuery();
+        HashMap<String, Integer> result = new HashMap<>();
 
-    //TODO:
-    //public static ArrayList getAllCountries(){}
+        //this is very hacky, and should probably be done better eventually
+        ArrayList<String> days = new ArrayList<>();
+        int month = 1;
+        int day = 21;
+        while(month < 5 || day < 6){
+            day++;
+            if((day == 32 && month == 1) || (day == 29 && month == 2) || (day == 32 && month == 3)
+            || (day == 31 && month == 4)){
+                day = 1;
+                month++;
+            }
+            days.add("" + month + "/" + day + "/20");
+        }
+
+        while(rs.next()){
+            if(rs.getDouble("FIPS") == fips){
+                for(int i = 0; i < days.size(); i++){
+                    result.put(days.get(i), rs.getInt(days.get(i)));
+                }
+            }
+        }
+
+        return result;
+    }
 }
