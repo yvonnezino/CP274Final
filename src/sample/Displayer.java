f1bb463dccde22c7dc33d6073728db6234adf2e1
connorcopus <ccopus1297@gmail.com>
2020-05-08 21:13:27 -0400

    Gui updating worked on more

--- src/sample/Displayer.java
+++ src/sample/Displayer.java
@@ -2,10 +2,8 @@ package sample;
 
 import javafx.fxml.FXML;
 import javafx.fxml.Initializable;
-import javafx.scene.chart.XYChart;
-import javafx.scene.chart.LineChart;
-import javafx.scene.chart.NumberAxis;
-import javafx.scene.chart.PieChart;
+import javafx.scene.chart.*;
+import javafx.scene.control.Label;
 import javafx.scene.text.Text;
 import java.net.URL;
 import java.sql.SQLException;
@@ -13,10 +11,31 @@ import java.util.ArrayList;
 import java.util.ResourceBundle;
 
     public class Displayer implements Initializable {
+        DbTools tool = new DbTools();
+
+        @FXML
+        private Label deadNumber;
+        @FXML
+        private Label recoveredNumber;
+        @FXML
+        private Label infectedNumber;
+        @FXML
+        private LineChart lineChart;
+        @FXML
+        private CategoryAxis xAxis;
+        @FXML
+        private NumberAxis yAxis;
+        @FXML
+        private PieChart pieChart;
+
 
 
         @Override
         public void initialize(URL url, ResourceBundle resourceBundle) {
+            //Need to figure out how to get data out of the Arraylists for each county, state, country
+//            deadNumber.setText();
+//            recoveredNumber.setText();
+//            infectedNumber.setText();
 
         }
     }
