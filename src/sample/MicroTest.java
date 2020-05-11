389aea9cc7fba562aa119ca6ad8491ce20586651
connorcopus <ccopus1297@gmail.com>
2020-05-11 13:53:11 -0400

    Tests

---
+++ src/sample/MicroTest.java
@@ -0,0 +1,55 @@
+package sample;
+
+import org.junit.jupiter.api.Test;
+
+import java.sql.Connection;
+
+import static org.junit.jupiter.api.Assertions.assertEquals;
+import static org.junit.jupiter.api.Assertions.assertNotNull;
+
+import java.sql.Connection;
+import java.util.*;
+import org.junit.jupiter.api.Test;
+import static org.junit.jupiter.api.Assertions.assertEquals;
+import static org.junit.jupiter.api.Assertions.assertNotNull;
+
+public class MicroTest {
+    DbTools newconnection = new DbTools();
+
+    @Test
+    void testCases() throws Exception {
+        County first = newconnection
+                .getAllCounties()
+                .get(5);
+        int cases = first.getAll("confirmed").size();
+        assertEquals(100,cases);
+    }
+
+    @Test
+    void testDeaths() throws Exception {
+        County first = newconnection
+                .getAllCounties()
+                .get(5);
+        int cases = first.getAll("deaths").size();
+        assertEquals(100,cases);
+    }
+
+
+    @Test
+    void testCon() throws Exception {
+        Connection con = newconnection
+                .getConnection();
+        assertNotNull(con);
+    }
+
+    @Test
+    void testCounty() throws Exception {
+        County first = newconnection
+                .getAllCounties()
+                .get(5) ;
+        String name = first.name;
+        name.equals("Autauga");
+        }
+
+
+}
